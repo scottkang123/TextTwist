eecbecb4f31d0ff3b407b5ba751a34b4c295c7b3
scottkang123 <48114611+scottkang123@users.noreply.github.com>
2022-07-02 00:55:19 -0500

    Add files via upload

---
+++ project10/cmake-build-debug/CMakeFiles/project10.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project10.dir/Trie.cpp.obj"
+  "CMakeFiles/project10.dir/TrieNode.cpp.obj"
+  "CMakeFiles/project10.dir/TextTwist.cpp.obj"
+  "../project10.pdb"
+  "../project10.exe"
+  "../project10.exe.manifest"
+  "libproject10.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
